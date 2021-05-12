44d8363c8f145241fc5a0482262952fb74839620
mutation <mutation@naver.com>
2021-05-12 15:20:47 +0900

    0512 java

---
+++ 0506/src/ArrayExam.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ArrayExam {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] arr = {0,0,0,0,0};
+		int max=0,index=0;
+		
+		for(i=0; i<5; i++){
+			
+			
+		}
+		
+		System.out.println("가장큰 숫자는"+ "번쨰"+ "숫자인");
+		
+		
+		
+
+	}
+
+}
